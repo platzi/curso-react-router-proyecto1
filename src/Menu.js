d8c5e4739967be8a4c0cfe0dab63679b3451d1db
Juan David Castro <me@juandc.co>
2022-06-01 11:59:05 -0500

    menu auth

--- src/Menu.js
+++ src/Menu.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useAuth } from './auth';
 
 function Menu() {
+  const auth = useAuth();
+  
   return (
     <nav>
       <ul>
-        {routes.map(route => (
-          <li key={route.to}>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? 'red' : 'blue',
-              })}
-              to={route.to}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map(route => {
+          if (route.publicOnly && auth.user) return null;
+          if (route.private && !auth.user) return null;
+          
+          return (
+            <li key={route.to}>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? 'red' : 'blue',
+                })}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
@@ -26,22 +34,28 @@ const routes = [];
 routes.push({
   to: '/',
   text: 'Home',
+  private: false,
 });
 routes.push({
   to: '/blog',
   text: 'Blog',
+  private: false,
 });
 routes.push({
   to: '/profile',
   text: 'Profile',
+  private: true,
 });
 routes.push({
   to: '/login',
   text: 'Login',
+  private: false,
+  publicOnly: true,
 });
 routes.push({
   to: '/logout',
   text: 'Logout',
+  private: true,
 });
 
 export { Menu };
