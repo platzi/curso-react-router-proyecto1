7cd88a93dd209fa78b8991be1a138c5c143039bb
Juan David Castro <me@juandc.co>
2022-06-01 12:57:21 -0500

    roles

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useAuth } from './auth';
 import { blogdata } from './blogdata'
 
 function BlogPost() {
   const navigate = useNavigate();
   const { slug } = useParams();
 
+  const auth = useAuth();
+
   const blogpost = blogdata.find(post => post.slug === slug);
+
+  const canDelete = auth.user?.isAdmin || blogpost.author === auth.user?.username;
 
   const returnToBlog = () => {
     navigate('/blog');
@@ -18,6 +23,10 @@ function BlogPost() {
       <button onClick={returnToBlog}>Volver al blog</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
+
+      {canDelete && (
+        <button>Eliminar blogpost</button>
+      )}
     </>
   );
 }
--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 
+const adminList = ['Irisval', 'RetaxMaster', 'freddier'];
+
 const AuthContext = React.createContext();
 
 function AuthProvider({ children }) {
@@ -8,7 +10,8 @@ function AuthProvider({ children }) {
   const [user, setUser] = React.useState(null);
 
   const login = ({ username }) => {
-    setUser({ username });
+    const isAdmin = adminList.find(admin => admin === username);
+    setUser({ username, isAdmin });
     navigate('/profile');
   };
   
