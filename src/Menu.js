2248379f0ce68822eae764262b7be35531b01474
Juan David Castro <me@juandc.co>
2022-06-01 10:08:44 -0500

    Link vs. NavLink

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 function Menu() {
   return (
-    <nav>Menu</nav>
+    <nav>
+      <ul>
+        {routes.map(route => (
+          <li>
+            <NavLink
+              style={({ isActive }) => ({
+                color: isActive ? 'red' : 'blue',
+              })}
+              to={route.to}
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+        
+        {/* <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li> */}
+
+        {/* <li>
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'blue',
+            })}
+            to="/"
+          >Home</NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'blue',
+            })}
+            to="/blog"
+          >Blog</NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'blue',
+            })}
+            to="/profile"
+          >Profile</NavLink>
+        </li> */}
+      </ul>
+    </nav>
   );
 }
+
+const routes = [];
+routes.push({
+  to: '/',
+  text: 'Home',
+});
+routes.push({
+  to: '/blog',
+  text: 'Blog',
+});
+routes.push({
+  to: '/profile',
+  text: 'Profile',
+});
 
 export { Menu };
